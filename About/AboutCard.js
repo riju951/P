3087098baef9cf7e522a02e305986ee2b7d6948b
Rijans Bhagat <82694741+riju951@users.noreply.github.com>
2023-08-24 02:29:31 +0530

    Add files via upload

---
+++ About/AboutCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Rijans Bhagat </span>
+            from <span className="purple"> Bhubaneswar, India.</span>
+            <br /> I am a final year student pursuing an Bachlear of Technology (B.Tech)
+            in Information and Communication.
+            <br />
+            I am currently enrolled in Pandit Deendayal Energy
+            University
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Photography
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writing Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Soumyajit</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
